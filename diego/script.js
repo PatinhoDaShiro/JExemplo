ba2e916b5cae8db08b9ad61d29777fa22751230a
PatinhoDaShiro <81689830+PatinhoDaShiro@users.noreply.github.com>
2022-10-21 11:31:38 -0300

    Add files via upload

--- diego/script.js
+++ diego/script.js
@@ -4,6 +4,7 @@ let resultado=document.querySelector("#result")
 function vezesdois(){
     resultado.innerHTML="Quadrado: ";
     resultado.append(numero.value*numero.value)
+    numero.value=""
 }
 
 //Cinco numeros
@@ -27,5 +28,50 @@ function cincovezes(){
         }
       }
 }
+//IDADES
+let idade=document.getElementById("idadeInput")
+let menores=document.getElementById("menores")
+let maiores=document.getElementById("maiores")
+cont1=0;
+cont2=0;
+function idades(){
+    if(idade.value>=18){
+        cont1++
+        maiores.innerHTML="Há um total de "+cont1+" pessoas com idade maiores que 18 anos"
+    }
+    else if(idade.value<18 && idade.value>0){
+        cont2++
+        menores.innerHTML="Há um total de "+cont2+" pessoas com idade menores que 18 anos"
+    }
+    else {
+        alert("idade invalida")
+    }
+}
+function reiniciar(){
+    cont1=0
+    cont2=0
+    menores.innerHTML=""
+    maiores.innerHTML=""  
+}
+//Nomes invertidos
+let nick=document.getElementById("nome")
+let aviso=document.getElementById("aviso")
+let lista =document.getElementById("lista")
+let nomes=[]
+function addList(){
+    nomes.push(nick.value)
+    aviso.innerHTML="Você possui um total de "+nomes.length+" nomes adicionados."
+}
+function mLista(){
+aviso.innerHTML="Sua lista invertida"
+
+
+for(i=nomes.length-1;i>=+0;i--){
+
+   lista.append(nomes[i]+" ")
+}
+}
+
+
 
 
